098dccb8d2464c8a599800478eeae47046b4528d
TheFloW <theofficialflow1996@gmail.com>
2017-06-13 16:34:40 +0200

    Release v2

--- main.c
+++ main.c
@@ -31,6 +31,8 @@ PSP_MODULE_INFO("GTARemasteredControls", 0x1007, 1, 0);
 
 int sceKernelQuerySystemCall(void *function);
 
+#define MAKE_CALL(a, f) _sw(0x0C000000 | (((u32)(f) >> 2) & 0x03FFFFFF), a);
+
 #define REDIRECT_FUNCTION(a, f) \
 { \
 	u32 func = a; \
@@ -38,15 +40,20 @@ int sceKernelQuerySystemCall(void *function);
 	_sw(0, func + 4); \
 }
 
-#define MAX_VALUE 0xFF
-#define CENTER 0x80
-#define DEADZONE 0x20
-
-#define FACTOR 1.2f
+enum GtaPad {
+	PAD_LX = 1,
+	PAD_LY = 2,
+	PAD_RX = 3,
+	PAD_RY = 4,
+	PAD_RTRIGGER = 7,
+	PAD_CROSS = 21,
+};
 
 STMOD_HANDLER previous;
 
-u32 MakeSyscallStub(void *function) {
+static u32 vcsAccelerationNormalStub;
+
+static u32 MakeSyscallStub(void *function) {
 	SceUID block_id = sceKernelAllocPartitionMemory(PSP_MEMORY_PARTITION_USER, "", PSP_SMEM_High, 2 * sizeof(u32), NULL);
 	u32 stub = (u32)sceKernelGetBlockHeadAddr(block_id);
 	_sw(0x03E00008, stub);
@@ -54,103 +61,184 @@ u32 MakeSyscallStub(void *function) {
 	return stub;
 }
 
-int cameraX() {
-	int k1 = pspSdkSetK1(0);
+short cameraX(short *pad) {
+	return pad[PAD_RX];
+}
 
-	SceCtrlData pad;
-	sceCtrlPeekBufferPositive(&pad, 1);
+short cameraY(short *pad) {
+	return pad[PAD_RY];
+}
 
-	char rx = pad.Rsrv[0] - CENTER;
-	if (rx < -DEADZONE || rx > DEADZONE) {
-		pspSdkSetK1(k1);
-		return (int)((float)rx * FACTOR);
-	}
+short aimX(short *pad) {
+	return pad[PAD_LX] ? pad[PAD_LX] : pad[PAD_RX];
+}
 
-	pspSdkSetK1(k1);
+short aimY(short *pad) {
+	return pad[PAD_LY] ? pad[PAD_LY] : pad[PAD_RY];
+}
+
+short vcsAcceleration(short *pad) {
+	if (pad[77] == 0)
+		return pad[PAD_RTRIGGER];
 	return 0;
 }
 
-int cameraY() {
-	int k1 = pspSdkSetK1(0);
-
-	SceCtrlData pad;
-	sceCtrlPeekBufferPositive(&pad, 1);
-
-	char ry = MAX_VALUE - pad.Rsrv[1] - CENTER;
-	if (ry < -DEADZONE || ry > DEADZONE) {
-		pspSdkSetK1(k1);
-		return (int)((float)ry * FACTOR);
-	}
-
-	pspSdkSetK1(k1);
+short vcsAccelerationNormal(short *pad) {
+	if (pad[77] == 0)
+		return pad[PAD_CROSS];
 	return 0;
 }
 
-int aimX() {
-	int k1 = pspSdkSetK1(0);
-
-	SceCtrlData pad;
-	sceCtrlPeekBufferPositive(&pad, 1);
-
-	char lx = pad.Lx - CENTER;
-	if (lx < -DEADZONE || lx > DEADZONE) {
-		pspSdkSetK1(k1);
-		return (int)((float)lx * FACTOR);
-	}
-
-	char rx = pad.Rsrv[0] - CENTER;
-	if (rx < -DEADZONE || rx > DEADZONE) {
-		pspSdkSetK1(k1);
-		return (int)((float)rx * FACTOR);
-	}
-
-	pspSdkSetK1(k1);
+short lcsAcceleration(short *pad) {
+	if (pad[67] == 0)
+		return pad[PAD_RTRIGGER];
 	return 0;
 }
 
-int aimY() {
-	int k1 = pspSdkSetK1(0);
-
-	SceCtrlData pad;
-	sceCtrlPeekBufferPositive(&pad, 1);
-
-	char ly = MAX_VALUE - pad.Ly - CENTER;
-	if (ly < -DEADZONE || ly > DEADZONE) {
-		pspSdkSetK1(k1);
-		return (int)((float)ly * FACTOR);
+int PatchVCS(u32 addr, u32 text_addr) {
+	// Implement right analog stick
+	if (_lw(addr) == 0x10000006 && _lw(addr + 0x4) == 0xA3A70003) {
+		_sw(0x10000003, addr + 0x00);
+		_sw(0x93A6000E, addr + 0x10); // lbu $a2, 14($sp)
+		_sw(0x93A7000F, addr + 0x14); // lbu $a3, 15($sp)
+		_sw(0x00000000, addr + 0x18); // nop
+		_sw(0xA3A60000, addr + 0x1C); // sb $a2, 0($sp)
+		_sw(0xA3A70001, addr + 0x20); // sb $a3, 1($sp)
+		_sw(0x8E060054, addr + 0x24); // lw $a2, 84($s0)
+		return 1;
 	}
 
-	char ry = MAX_VALUE - pad.Rsrv[1] - CENTER;
-	if (ry < -DEADZONE || ry > DEADZONE) {
-		pspSdkSetK1(k1);
-		return (int)((float)ry * FACTOR);
+	// Redirect camera movement
+	if (_lw(addr) == 0x14800036 && _lw(addr + 0x10) == 0x10400016) {
+		_sw(0x00000000, addr + 0x00);
+		_sw(0x10000016, addr + 0x10);
+		MAKE_CALL(addr + 0x8C, MakeSyscallStub(cameraX));
+		_sw(0x00000000, addr + 0x108);
+		_sw(0x10000002, addr + 0x118);
+		MAKE_CALL(addr + 0x144, MakeSyscallStub(cameraY));
+		return 1;
 	}
 
-	pspSdkSetK1(k1);
+	// Redirect gun aim movement
+	if (_lw(addr) == 0x04800040 && _lw(addr + 0x8) == 0x1080003E) {
+		u32 aimXStub = MakeSyscallStub(aimX);
+		u32 aimYStub = MakeSyscallStub(aimY);
+		MAKE_CALL(addr + 0x50, aimXStub);
+		MAKE_CALL(addr + 0x7C, aimXStub);
+		MAKE_CALL(addr + 0x8C, aimXStub);
+		MAKE_CALL(addr + 0x158, aimYStub);
+		MAKE_CALL(addr + 0x1BC, aimYStub);
+		return 1;
+	}
+
+	// Swap R trigger and cross button
+	if (_lw(addr + 0x00) == 0x9485009A && _lw(addr + 0x04) == 0x0005282B &&
+		_lw(addr + 0x08) == 0x30A500FF && _lw(addr + 0x0C) == 0x10A00003 &&
+		_lw(addr + 0x10) == 0x00000000 && _lw(addr + 0x14) == 0x10000002 &&
+		_lw(addr + 0x18) == 0x00001025 && _lw(addr + 0x1C) == 0x8482002A) {
+		REDIRECT_FUNCTION(addr, MakeSyscallStub(vcsAcceleration));
+		return 1;
+	}
+
+	// Use normal button for flying plane
+	if (_lw(addr) == 0xC60E0780 && _lw(addr + 0x4) == 0x460D6302 && _lw(addr + 0x8) == 0x460D7342) {
+		MAKE_CALL(addr + 0x1C, vcsAccelerationNormalStub);
+		MAKE_CALL(addr + 0x3D0, vcsAccelerationNormalStub);
+		return 1;
+	}
+
+	// Use normal button for flying helicoper
+	if (_lw(addr) == 0x16400018 && _lw(addr + 0xC) == 0x02202025) {
+		MAKE_CALL(addr + 0x14, vcsAccelerationNormalStub);
+		return 1;
+	}
+
+	if (_lw(addr) == 0x1480000C && _lw(addr + 0x10) == 0x50A0000A) {
+		_sh(PAD_RTRIGGER * 2, addr + 0x20);
+		_sh(PAD_RTRIGGER * 2, addr + 0x68);
+		_sh(PAD_CROSS * 2, addr + 0x80);
+		return 1;
+	}
+
+	// Allow using L trigger when walking
+	if (_lw(addr) == 0x1480000E && _lw(addr + 0x10) == 0x10800008 && _lw(addr + 0x1C) == 0x04800003) {
+		_sw(0, addr + 0x10);
+		_sw(0, addr + 0x9C);
+		return 1;
+	}
+
+	// Force L trigger value in the L+camera movement function
+	if (_lw(addr) == 0x84C7000A) {
+		_sw(0x2407FFFF, addr);
+		return 1;
+	}
+
 	return 0;
 }
 
-int crossPressedLCS(void *a0) {
-	int k1 = pspSdkSetK1(0);
-
-	if (((u16 *)a0)[67] == 0) {
-		pspSdkSetK1(k1);
-		return ((u16 *)a0)[7];
+int PatchLCS(u32 addr, u32 text_addr) {
+	// Implement right analog stick
+	if (_lw(addr) == 0x10000006 && _lw(addr + 0x4) == 0xA3A70013) {
+		_sw(0x10000003, addr + 0x00);
+		_sw(0x93A6001E, addr + 0x10); // lbu $a2, 30($sp)
+		_sw(0x93A7001F, addr + 0x14); // lbu $a3, 31($sp)
+		_sw(0x00000000, addr + 0x18); // nop
+		_sw(0xA3A60010, addr + 0x1C); // sb $a2, 16($sp)
+		_sw(0xA3A70011, addr + 0x20); // sb $a3, 17($sp)
+		_sw(0x8E060054, addr + 0x24); // lw $a2, 84($s0)
+		return 1;
 	}
 
-	pspSdkSetK1(k1);
-	return 0;
-}
-
-int crossPressedVCS(void *a0) {
-	int k1 = pspSdkSetK1(0);
-
-	if (((u16 *)a0)[77] == 0) {
-		pspSdkSetK1(k1);
-		return ((u16 *)a0)[7];
+	// Redirect camera movement
+	if (_lw(addr) == 0x14800034 && _lw(addr + 0x10) == 0x10400014) {
+		_sw(0x00000000, addr + 0x00);
+		_sw(0x10000014, addr + 0x10);
+		MAKE_CALL(addr + 0x84, MakeSyscallStub(cameraX));
+		_sw(0x00000000, addr + 0x100);
+		_sw(0x10000002, addr + 0x110);
+		MAKE_CALL(addr + 0x13C, MakeSyscallStub(cameraY));
+		return 1;
 	}
 
-	pspSdkSetK1(k1);
+	// Redirect gun aim movement
+	if (_lw(addr) == 0x04800036 && _lw(addr + 0x8) == 0x10800034) {
+		u32 aimXStub = MakeSyscallStub(aimX);
+		u32 aimYStub = MakeSyscallStub(aimY);
+		MAKE_CALL(addr + 0x3C, aimXStub);
+		MAKE_CALL(addr + 0x68, aimXStub);
+		MAKE_CALL(addr + 0x78, aimXStub);
+		MAKE_CALL(addr + 0x130, aimYStub);
+		MAKE_CALL(addr + 0x198, aimYStub);
+		return 1;
+	}
+
+	// Swap R trigger and cross button
+	if (_lw(addr + 0x00) == 0x94850086 && _lw(addr + 0x04) == 0x10A00003 &&
+		_lw(addr + 0x08) == 0x00000000 && _lw(addr + 0x0C) == 0x10000002 &&
+		_lw(addr + 0x10) == 0x00001025 && _lw(addr + 0x14) == 0x8482002A) {
+		REDIRECT_FUNCTION(addr, MakeSyscallStub(lcsAcceleration));
+		return 1;
+	}
+
+	if (_lw(addr) == 0x14A0000E && _lw(addr + 0x10) == 0x50C0000C) {
+		_sh(PAD_RTRIGGER * 2, addr + 0x20);
+		_sh(PAD_CROSS * 2, addr + 0x50);
+		return 1;
+	}
+
+	// Allow using L trigger when walking
+	if (_lw(addr) == 0x14A0000E && _lw(addr + 0x10) == 0x10A00008 && _lw(addr + 0x1C) == 0x04A00003) {
+		_sw(0, addr + 0x10);
+		_sw(0, addr + 0x74);
+		return 1;
+	}
+
+	// Force L trigger value in the L+camera movement function
+	if (_lw(addr) == 0x850A000A) {
+		_sw(0x240AFFFF, addr);
+		return 1;
+	}
+
 	return 0;
 }
 
@@ -158,118 +246,22 @@ int OnModuleStart(SceModule2 *mod) {
 	char *modname = mod->modname;
 	u32 text_addr = mod->text_addr;
 
+	int gta_version = -1;
+
 	if (strcmp(modname, "GTA3") == 0) {
+		vcsAccelerationNormalStub = MakeSyscallStub(vcsAccelerationNormal);
+
 		u32 i;
 		for (i = 0; i < mod->text_size; i += 4) {
 			u32 addr = text_addr + i;
 
-			// Redirect camera movement
-			if (_lw(addr) == 0x14800036 && _lw(addr + 0x10) == 0x10400016) {
-				// log("VCS CAMERA: 0x%08X, 0x%08X\n", addr - 0x18 - text_addr, addr - 0x18 + 0x108 - text_addr);
-				REDIRECT_FUNCTION(addr - 0x18, MakeSyscallStub(cameraX));
-				REDIRECT_FUNCTION(addr - 0x18 + 0x108, MakeSyscallStub(cameraY));
+			if ((gta_version == -1 || gta_version == 0) && PatchVCS(addr, text_addr)) {
+				gta_version = 0;
 				continue;
 			}
 
-			// Redirect gun aim movement x
-			if (_lw(addr) == 0x04800040 && _lw(addr + 0x8) == 0x1080003E) {
-				// log("VCS AIM X: 0x%08X\n", addr - 0x14 - text_addr);
-				REDIRECT_FUNCTION(addr - 0x14, MakeSyscallStub(aimX));
-				continue;
-			}
-
-			// Redirect gun aim movement y
-			if (_lw(addr) == 0x0480003F && _lw(addr + 0x8) == 0x1080003D) {
-				// log("VCS AIM Y: 0x%08X\n", addr - 0x14 - text_addr);
-				REDIRECT_FUNCTION(addr - 0x14, MakeSyscallStub(aimY));
-				continue;
-			}
-
-			// Swap R trigger and cross button
-			if (_lw(addr + 0x00) == 0x9485009A && _lw(addr + 0x04) == 0x0005282B &&
-				_lw(addr + 0x08) == 0x30A500FF && _lw(addr + 0x0C) == 0x10A00003 &&
-				_lw(addr + 0x10) == 0x00000000 && _lw(addr + 0x14) == 0x10000002 &&
-				_lw(addr + 0x18) == 0x00001025 && _lw(addr + 0x1C) == 0x8482002A) {
-				// log("VCS CROSS: 0x%08X\n", addr - text_addr);
-				REDIRECT_FUNCTION(addr, MakeSyscallStub(crossPressedVCS));
-				continue;
-			}
-
-			if (_lw(addr) == 0x1480000C && _lw(addr + 0x10) == 0x50A0000A) {
-				// log("VCS CROSS/R: 0x%08X, 0x%08X\n", addr + 0x20 - text_addr, addr + 0x50 - text_addr);
-				_sh(14, addr + 0x20);
-				_sh(14, addr + 0x68);
-				_sh(42, addr + 0x80);
-				continue;
-			}
-
-			// Allow using L trigger when walking
-			if (_lw(addr) == 0x1480000E && _lw(addr + 0x10) == 0x10800008 && _lw(addr + 0x1C) == 0x04800003) {
-				// log("VCS WALK: 0x%08X, 0x%08X\n", addr + 0x10 - text_addr, addr + 0x74 - text_addr);
-				_sw(0, addr + 0x10);
-				_sw(0, addr + 0x9C);
-				continue;
-			}
-
-			// Force L trigger value in the L+camera movement function
-			if (_lw(addr) == 0x84C7000A) {
-				// log("VCS L+CAMERA: 0x%08X\n", addr - text_addr);
-				_sw(0x2407FFFF, addr);
-				continue;
-			}
-
-			///////////////////////////////////////////////////////////////////////////////////////////////
-
-			// Redirect camera movement
-			if (_lw(addr) == 0x14800034 && _lw(addr + 0x10) == 0x10400014) {
-				// log("LCS CAMERA: 0x%08X, 0x%08X\n", addr - 0x18 - text_addr, addr - 0x18 + 0x100 - text_addr);
-				REDIRECT_FUNCTION(addr - 0x18, MakeSyscallStub(cameraX));
-				REDIRECT_FUNCTION(addr - 0x18 + 0x100, MakeSyscallStub(cameraY));
-				continue;
-			}
-
-			// Redirect gun aim movement x
-			if (_lw(addr) == 0x04800036 && _lw(addr + 0x8) == 0x10800034) {
-				// log("LCS AIM X: 0x%08X\n", addr - 0x14 - text_addr)
-				REDIRECT_FUNCTION(addr - 0x14, MakeSyscallStub(aimX));
-				continue;
-			}
-
-			// Redirect gun aim movement y
-			if (_lw(addr) == 0x04800041 && _lw(addr + 0x8) == 0x1080003F) {
-				// log("LCS AIM Y: 0x%08X\n", addr - 0x14 - text_addr)
-				REDIRECT_FUNCTION(addr - 0x14, MakeSyscallStub(aimY));
-				continue;
-			}
-
-			// Swap R trigger and cross button
-			if (_lw(addr + 0x00) == 0x94850086 && _lw(addr + 0x04) == 0x10A00003 &&
-				_lw(addr + 0x08) == 0x00000000 && _lw(addr + 0x0C) == 0x10000002 &&
-				_lw(addr + 0x10) == 0x00001025 && _lw(addr + 0x14) == 0x8482002A) {
-				// log("LCS CROSS: 0x%08X\n", addr - text_addr);
-				REDIRECT_FUNCTION(addr, MakeSyscallStub(crossPressedLCS));
-				continue;
-			}
-
-			if (_lw(addr) == 0x14A0000E && _lw(addr + 0x10) == 0x50C0000C) {
-				// log("LCS CROSS/R: 0x%08X, 0x%08X\n", addr + 0x20 - text_addr, addr + 0x50 - text_addr);
-				_sh(14, addr + 0x20);
-				_sh(42, addr + 0x50);
-				continue;
-			}
-
-			// Allow using L trigger when walking
-			if (_lw(addr) == 0x14A0000E && _lw(addr + 0x10) == 0x10A00008 && _lw(addr + 0x1C) == 0x04A00003) {
-				// log("LCS WALK: 0x%08X, 0x%08X\n", addr + 0x10 - text_addr, addr + 0x74 - text_addr);
-				_sw(0, addr + 0x10);
-				_sw(0, addr + 0x74);
-				continue;
-			}
-
-			// Force L trigger value in the L+camera movement function
-			if (_lw(addr) == 0x850A000A) {
-				// log("LCS L+CAMERA: 0x%08X\n", addr - text_addr);
-				_sw(0x240AFFFF, addr);
+			if ((gta_version == -1 || gta_version == 1) && PatchLCS(addr, text_addr)) {
+				gta_version = 1;
 				continue;
 			}
 		}
--- readme.txt
+++ readme.txt
@@ -1,7 +1,11 @@
-GTA Remastered Controls Patch
+GTA Remastered Controls Patch v2
 
 Put 'gta_remastered.prx' at 'ux0:pspemu/seplugins/gta_remastered.prx' and write this line to 'ux0:pspemu/seplugins/game.txt': 'ms0:/seplugins/gta_remastered.prx 1' (Without ')
 
 This patch has been tested for GTA LCS German V1 and GTA VCS German V1. They should work for other regions too. If not, please fix it for me.
 
 Note that this patch does swap R and X for driving. Now you can accelerate with R and use handbrake with X. This way, the use of the right analog stick is cooler
+
+Changelog v2
+- Fixed bug where flying planes and helicopters didn't work, because the R trigger was both used for acceleration and rotation.
+- Fixed bug where pressing L trigger for slow aiming didn't work.
